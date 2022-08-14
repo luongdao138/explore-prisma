5540802d3d363e46670cb8ebd5b7689140bcbb05
Luong Dao <luongdv85025@co-well.vn>
2022-08-14 12:30:47 +0700

    add api

--- server/app.ts
+++ server/app.ts
@@ -1,31 +1,93 @@
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import { PrismaClient } from '@prisma/client'
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { PrismaClient } from '@prisma/client';
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
+const prisma = new PrismaClient();
 
-app.use(cors({ origin: [process.env.CLIENT_URL || ''], credentials: true }))
-
+app.use(express.json());
+app.use(cors({ origin: [process.env.CLIENT_URL || ''], credentials: true }));
 
 app.get('/posts', async (req, res) => {
-       const prisma = new PrismaClient()
+  const posts = await prisma.post.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  });
 
-       const posts = await prisma.post.findMany({
-              select: {
-                      id: true, 
-                      title: true
-              }
-        })
-   
+  return res.status(200).json({ data: posts });
+});
 
-       return res.status(200).json({  data: posts})
-})
+app.get('/posts/:id', async (req, res) => {
+  const { id } = req.params;
+  const post = await prisma.post.findUnique({
+    where: { id },
+    select: {
+      id: true,
+      title: true,
+      createdAt: true,
+      body: true,
+      author: {
+        select: {
+          id: true,
+          name: true,
+          avatar: true,
+        },
+      },
+      comments: {
+        select: {
+          message: true,
+          id: true,
+          parentId: true,
+          createdAt: true,
+          user: {
+            select: {
+              id: true,
+              name: true,
+              avatar: true,
+            },
+          },
+        },
+      },
+    },
+  });
 
+  return res.status(200).json({ data: post });
+});
 
-const PORT = process.env.PORT || 5000
+app.post('/posts/:id/comments', async (req, res) => {
+  const { postId, message, parentId } = req.body;
+
+  const newComment = await prisma.comment.create({
+    data: {
+      message,
+      userId: '6cb7c2fa-fb14-43d9-993b-28978f44b422',
+      postId,
+      parentId,
+    },
+    select: {
+      id: true,
+      message: true,
+      parentId: true,
+      user: {
+        select: {
+          id: true,
+          avatar: true,
+          name: true,
+        },
+      },
+      createdAt: true,
+    },
+  });
+
+  return res.status(201).json({ data: newComment });
+});
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-      console.log(`server listening on port ${PORT}`)
-})
+  console.log(`server listening on port ${PORT}`);
+});
--- server/script.ts
+++ server/script.ts
@@ -1,99 +1,93 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
 
 const main = async () => {
-     //    await prisma.user.createMany({
-     //          data: [
-     //             {
-     //                  name: 'Dao Van Luong'
-     //             },
-     //             {
-     //                  name: 'Nguyen Trong Phong'
-     //             },
-     //             {
-     //                  name: 'Dao Van Quan'
-     //             },
-     //          ]
-     //    })
+  // Insert user
+  //   await prisma.user.createMany({
+  //     data: [
+  //       {
+  //         name: 'luongdao',
+  //         avatar: 'https://randomuser.me/api/portraits/men/32.jpg',
+  //       },
+  //       {
+  //         name: 'phongnguyen',
+  //         avatar:
+  //           'https://images.unsplash.com/photo-1456327102063-fb5054efe647?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=f05c14dd4db49f08a789e6449604c490',
+  //       },
+  //       {
+  //         name: 'quandao',
+  //         avatar:
+  //           'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=046c29138c1335ef8edee7daf521ba50',
+  //       },
+  //     ],
+  //   });
+  const users = await prisma.user.findMany();
+  console.log({ users });
 
-     // await prisma.user.deleteMany()
+  //    Insert post
+  //   await prisma.post.create({
+  //     data: {
+  //       body: `[Jeff Passan] Fernando Tatis Jr. will be suspended for 80 games, for violating MLB PED policy, MLB announced. The suspension will cover the remainder of this season and will render him ineligible for the postseason. It will continue into the beginning of next year.`,
+  //       title: 'Fernando Tatis Jr. will be suspended for 80 games',
+  //       authorId: '6cb7c2fa-fb14-43d9-993b-28978f44b422',
+  //       comments: {
+  //         createMany: {
+  //           data: [
+  //             {
+  //               message: `This is big, this type of thing can change a young man's career`,
+  //               userId: '891b50c0-52bb-4bba-b871-1cb39f7654c7',
+  //             },
+  //             {
+  //               message: `He's claiming it was in a medication he took to cure ringworm. Like he couldn't go to CVS and take Lotrimin like everyone else?`,
+  //               userId: '6a44e392-ea80-40a6-9334-5dedb9f79e3f',
+  //             },
+  //           ],
+  //         },
+  //       },
+  //     },
+  //   });
 
-     //   const users = await prisma.user.findMany({  
-     //        select: { id: true, name: true }
-     //   })
+  //    Insert comments
+  //   await prisma.comment.createMany({
+  //     data: [
+  //       {
+  //         message: 'He is signed for another 12 YEARS',
+  //         postId: 'b40e17cd-5535-464b-8850-d6b3388d963c',
+  //         userId: '6cb7c2fa-fb14-43d9-993b-28978f44b422',
+  //         parentId: 'daeaa212-4316-491a-b5ae-38bb2e3ba8e6',
+  //       },
+  //       {
+  //         message: 'Now THIS is Padresing!',
+  //         postId: 'b40e17cd-5535-464b-8850-d6b3388d963c',
+  //         userId: '6a44e392-ea80-40a6-9334-5dedb9f79e3f',
+  //         parentId: 'daeaa212-4316-491a-b5ae-38bb2e3ba8e6',
+  //       },
+  //     ],
+  //   });
 
-     //   console.log({users})
+  //   await prisma.comment.update({
+  //     where: {
+  //       id: 'aa38ab34-9e59-4482-a012-4110aa0543dc',
+  //     },
+  //     data: {
+  //       message: `don't they have clauses for this?`,
+  //     },
+  //   });
+  //   await prisma.comment.update({
+  //     where: {
+  //       id: '85360a14-8f67-4749-b5ab-0d3b4f7da189',
+  //     },
+  //     data: {
+  //       message: `Dude, the Browns gave a quarter-billion dollar contract to a known sexual predator, who just admitted it today`,
+  //     },
+  //   });
+};
 
-     //    await prisma.post.create({ 
-     //        data: {
-     //           title: 'This is post 1',
-     //           body: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
-     //           authorId: '07beab1e-5345-4ab3-9bb4-8130fd8f35b0',
-     //           comments: {
-     //                 createMany: {
-     //                      data: [
-     //                           {
-     //                               message: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC',
-     //                               userId: '07beab1e-5345-4ab3-9bb4-8130fd8f35b0'
-     //                           }
-     //                      ]
-     //                 }
-     //           }
-     //        },
-     //    })
-
-     //    await prisma.post.create({ 
-     //        data: {
-     //           title: 'This is post2',
-     //           body: `There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.`,
-     //           authorId: '53ea115f-86da-45ae-b7cc-ff55fb83cd06',
-     //           comments: {
-     //                 createMany: {
-     //                      data: [
-     //                           {
-     //                               message: 'This book is a treatise on the theory of ethics, very popular during the Renaissance',
-     //                               userId: '53ea115f-86da-45ae-b7cc-ff55fb83cd06'
-     //                           }
-     //                      ]
-     //                 }
-     //           }
-     //        },
-     //    })
-
-     //    await prisma.post.createMany({ data: [
-     //          {
-     //             title: 'This is post 3',
-     //             body: `The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.`,
-     //             authorId: 'ee7cb0b6-82c1-4b6e-b015-970f824fec50'
-     //          },
-     //    ] })
-
-     // await prisma.comment.createMany({
-     //       data: [
-     //          {
-     //            message: `But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born`,
-     //            postId: 'a63082a5-3719-4ef3-91fb-d008eb0479c5',
-     //            userId: '07beab1e-5345-4ab3-9bb4-8130fd8f35b0',
-     //            parentId: '540d7bcd-1c61-49e1-ab85-7062051775cf'
-     //          },
-     //          {
-     //            message: `At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos do`,
-     //            postId: 'a63082a5-3719-4ef3-91fb-d008eb0479c5',
-     //            userId: 'ee7cb0b6-82c1-4b6e-b015-970f824fec50',
-     //            parentId: '540d7bcd-1c61-49e1-ab85-7062051775cf'
-     //          },
-     //       ]
-     // })
-
-//      await prisma.comment.delete({  where: {
-//            id: 'da3a0c10-2762-4a60-87b9-fd2a9323eb3b'
-//      } })
-}
-
-
-main().then(() => {
-     console.log('Seed success')
-}).catch((error) => {
-      console.log(error)
-})
+main()
+  .then(() => {
+    console.log('Seed success');
+  })
+  .catch((error) => {
+    console.log(error);
+  });
